7f6fbfbce50d15b83307f9b601bd7dc353be8b2e
zaynOm <ouaziz0626@gmail.com>
2023-03-18 00:40:15 +0100

    adding 5-print_numbers.c :zero: to :nine:

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	short i = 0;
+
+	while (i < 10)
+	{
+		printf("%d", i);
+		i++;
+	}
+	printf("\n");
+
+	return (0);
+}
