fbc12633299b85f20f1f0b1a63f8ec57dd4e7ab9
zaynOm <ouaziz0626@gmail.com>
2023-03-17 02:52:21 +0100

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include<stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: This time 1 (Success)
+ */
+int main(void)
+{
+	system("echo 'and that piece of art is useful\" - Dora Korpar, 2015-10-19'");
+	return (1);
+}
