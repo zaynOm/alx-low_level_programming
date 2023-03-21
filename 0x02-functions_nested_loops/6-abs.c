49d0d6073ec4c68dae79202d94e858c9fced2f1b
zaynOm <ouaziz0626@gmail.com>
2023-03-21 22:12:58 +0000

    Task6: 6-abs.c :white_check_mark:

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+/**
+ * _abs - computes the absolute value of an integer
+ *@num: a decimal number
+ *
+ * Return: the absolute value of the given number
+ */
+int _abs(int num)
+{
+	if (num < 0)
+		return (num * -1);
+
+	return (num);
+}
