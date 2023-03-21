17fbaa742e5f06c52693d3d7a4c983194551212f
zaynOm <ouaziz0626@gmail.com>
2023-03-21 19:41:44 +0000

    Task2: 2-print_alphabet_x10.c :white_check_mark:

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints a-z 10 times
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		int j;
+
+		for (j = 97; j < 122; j++)
+			_putchar(j);
+
+		_putchar('\n');
+		i++;
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,5 +3,7 @@
 
 int _putchar(char);
 void print_alphabet(void);
+void print_alphabet_x10(void);
+
 
 #endif /* _MANI_H_ */
