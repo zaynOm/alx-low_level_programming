5a64621fc84e33416707fcd7ad0ed1d33268efb7
zaynOm <ouaziz0626@gmail.com>
2023-03-21 18:09:58 +0000

    Task:0 0-putchar.c :white_check_mark:

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,21 @@
 #include "main.h"
-/*
+/**
+ * main - Entry point
+ *
+ * Description: it outputs '_putchar'
+ *
+ * Return: Always (0)
  */
 int main(void)
 {
-        char word[8] = "_putchar";
-        int i = 0;
+	char word[8] = "_putchar";
+	int i = 0;
 
-        while(i < sizeof(word))
-        {
-                _putchar(word[i]);
-                i++;
-        }
-        _putchar('\n');
-
-        return (0);
-
+	while (i < (int)sizeof(word))
+	{
+		_putchar(word[i]);
+		i++;
+	}
+	_putchar('\n');
+	return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,1 +1,6 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
 int _putchar(char);
+
+#endif /* _MANI_H_ */
