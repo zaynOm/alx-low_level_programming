7fde3274829d82e0703c197cdb92d07720f1ca1c
zaynOm <ouaziz0626@gmail.com>
2023-03-22 00:56:08 +0000

    Task9: 9-times_table.c :white_check_mark:

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,36 +11,17 @@ void times_table(void)
 		_putchar(48);
 		for (j = 1; j < 10; j++)
 		{
+			_putchar(',');
+			_putchar(' ');
+
 			result = i * j;
 
-			if (j == 9 && i < 2)
-			{
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(result + 48);
-				_putchar('$');
-			}
-			else if (j == 9)
-			{
-				_putchar(' ');
+			if (result >= 10)
 				_putchar(result / 10 + 48);
-				_putchar(result % 10 + 48);
-				_putchar('$');
-			}
-			else if (result >= 10)
-			{
-				_putchar(' ');
-				_putchar(result / 10 + 48);
-				_putchar(result % 10 + 48);
-				_putchar(',');
-			}
 			else
-			{
 				_putchar(' ');
-				_putchar(' ');
-				_putchar(result + 48);
-				_putchar(',');
-			}
+
+			_putchar(result % 10 + 48);
 		}
 		_putchar('\n');
 	}
