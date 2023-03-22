ce88dbf09cc921c1381ce872ebae1af74bdfef18
zaynOm <ouaziz0626@gmail.com>
2023-03-22 01:01:23 +0000

    Task10: 10-add.c :white_check_mark:

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+/**
+ * add - adds two numbers and returns the result
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: the result of the addition
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
