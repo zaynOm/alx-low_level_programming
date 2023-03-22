657bce8b56ecba00cc14047823351531e991eaf3
zaynOm <ouaziz0626@gmail.com>
2023-03-22 10:30:37 +0000

    Task14: 102-fibonacci.c :heavy_check_mark:

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ *
+ */
+int main(void)
+{
+	unsigned long x = 1;
+	unsigned long y = 1;
+	unsigned long sum = 1;
+	int i;
+	
+	for (i = 0; i < 49; i++)
+	{
+		printf("%lu, ", sum);
+		sum = x + y;
+		x = y;
+		y = sum;
+	}
+	printf("%lu\n", sum);
+	
+	return (0);
+}
