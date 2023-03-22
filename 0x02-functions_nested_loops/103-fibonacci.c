27d5b47315b6a7a0341e0867bbe85317e7505753
zaynOm <ouaziz0626@gmail.com>
2023-03-22 11:41:15 +0000

    Task15: 103-fibonacci.c :heavy_check_mark:

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	unsigned int x = 0, y = 1, z = 0;
+	unsigned int sum;
+
+	while (z < 4000000)
+	{
+		z = x + y;
+		x = y;
+		y = z;
+		if (z % 2 == 0)
+			sum += z;
+	}
+	printf("%d\n", sum);
+}
