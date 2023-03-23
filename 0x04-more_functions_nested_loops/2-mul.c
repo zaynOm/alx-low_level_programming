d157a8b92bf7480b11b12b46b95b12514534358c
zaynOm <ouaziz0626@gmail.com>
2023-03-23 14:03:02 +0000

    Task2: 2-mul.c :white_check_mark:

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+/**
+ * mul - multiplies two integers
+ * @a: the first given integer
+ * @b: the second given integer
+ *
+ * Return: the result of multipling a and b.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
