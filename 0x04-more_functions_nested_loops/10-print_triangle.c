b58ac3f721da6a73103320fb822fd9b865ecc2e7
zaynOm <ouaziz0626@gmail.com>
2023-03-23 18:46:43 +0000

    Task10: 10-print_triangle.c :white_check_mark:

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_triangle - prints a triangle followed by a new line
+ * @size: the size of the triangle
+ */
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+		_putchar('\n');
+
+	for (i = 1 ; i <= size; i++)
+	{
+		for (j = 0; j < size - i; j++)
+		{
+			_putchar(' ');
+		}
+		for (j = 0; j < i; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
