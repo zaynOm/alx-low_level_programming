ea2f3cd0173842c00a9d6190d2675b437991abd3
zaynOm <ouaziz0626@gmail.com>
2023-03-23 02:04:02 +0000

    update

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,28 +1,48 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
+ * Desc: This program finds and prints the first 98 Fibonacci numbers,
+ * starting with 1 and 2
  *
- * Desc: this program prints the first 98 Fibonacci numbers
- * starting from 1 and 2
- *
- * Return: Always 0.
+ * Return: ALways 0
  */
 int main(void)
 {
-	long double x = 0;
-	long double y = 0.5;
-	long double z;
 	int i;
+	unsigned long int fib1, fib2, fib1_1, fib1_2, fib2_1, fib2_2, half1, half2;
 
-	for (i = 0; i < 98; i++)
+	fib1 = 1;
+	fib2 = 2;
+
+	printf("%lu", fib1);
+
+	for (i = 0; i < 90; i++)
 	{
-		z = x + y;
-		x = y;
-		y = z;
-		if (i == 97)
-			printf("%.Lf\n", z * 2);
-		else
-			printf("%.Lf, ", z * 2);
+		printf(", %lu", fib2);
+		fib2 = fib2 + fib1;
+		fib1 = fib2 - fib1;
 	}
+
+	fib1_1 = fib1 / 1000000000;
+	fib2_1 = fib2 / 1000000000;
+	fib1_2 = fib1 % 1000000000;
+	fib2_2 = fib2 % 1000000000;
+
+	for (i = 91; i < 98; ++i)
+	{
+		half1 = fib2_1 + (fib2_2 / 1000000000);
+		half2 =  fib2_2 % 1000000000;
+		printf(", %lu", half1);
+		printf("%lu", half2);
+
+		fib2_1 = fib2_1 + fib1_1;
+		fib1_1 = fib2_1 - fib1_1;
+		fib2_2 = fib2_2 + fib1_2;
+		fib1_2 = fib2_2 - fib1_2;
+	}
+
+	printf("\n");
+
 	return (0);
 }
