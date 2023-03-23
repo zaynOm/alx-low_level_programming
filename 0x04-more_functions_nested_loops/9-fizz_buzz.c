ba79b36ec92566443d2899efb1a990dd31f0d275
zaynOm <ouaziz0626@gmail.com>
2023-03-23 17:52:22 +0000

    Task9: 9-fizz_buzz.c :white_check_mark:

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Desc: prints numbers from 1 to 100, but "Fizz" for multiples of 3,
+ * "Buzz" for multiples of 5, "FizzBuzz" for multiples of both.
+ */
+void main(void)
+{
+	int i;
+
+	for (i = 1; i < 101; i++)
+	{
+		if (i % 15 == 0)
+			printf("FizzBuzz ");
+		else if (i % 3 == 0)
+			printf("Fizz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", i);
+	}
+	printf("\n");
+
+}
