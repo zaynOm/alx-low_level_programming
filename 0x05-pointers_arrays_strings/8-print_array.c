4d370d7468887cc58d535757ec2ca28f939d7ada
zaynOm <ouaziz0626@gmail.com>
2023-03-27 18:45:36 +0000

    Task8: 8-print_array.c :white_check_mark:

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * print_array - prints elements of an array of integers
+ * @a: the array of integers
+ * @n: number of elements of the array
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n - 1; i++)
+	{
+		printf("%d, ", a[i]);
+	}
+	printf("%d\n", a[n - 1]);
+}
