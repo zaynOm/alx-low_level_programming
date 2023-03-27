9e5984f3dbd53f63ff431d3fc9c5dd4bc9444a3c
zaynOm <ouaziz0626@gmail.com>
2023-03-27 16:44:19 +0000

    Task0: 0-reset_to_98.c :white_check_mark:

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,9 @@
+/**
+ * reset_to_98 - resets the value of the variable that the given pointer
+ * is pointing to to 98.
+ * @n: a pointer to an int variable
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
