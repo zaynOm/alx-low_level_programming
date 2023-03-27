2de39813a1758c0c293ddfd2a9a868be883196c6
zaynOm <ouaziz0626@gmail.com>
2023-03-27 23:06:07 +0000

    Task8: 8-print_array.c :white_check_mark:

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -8,11 +8,11 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	if (n > 0)
-	{
-		for (i = 0; i < n - 1; i++)
-			printf("%d, ", a[i]);
+	if (n < 0)
+		n *= -1;
 
-		printf("%d\n", a[n - 1]);
-	}
+	for (i = 0; i < n - 1; i++)
+		printf("%d, ", a[i]);
+
+	printf("%d\n", a[n - 1]);
 }
