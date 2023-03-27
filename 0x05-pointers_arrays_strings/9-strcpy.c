eb281bc5985cc0bf714b589950181c138007701f
zaynOm <ouaziz0626@gmail.com>
2023-03-27 23:35:44 +0000

    Task9: 9-strcpy.c :white_check_mark:

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+/**
+ * _strcpy - copies the string pointed to by src
+ * including the terminating null byte (\0), to the buffer pointed to by dest
+ *
+ * @dest: array of chars
+ * @src: a given string
+ *
+ * Return: the pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
