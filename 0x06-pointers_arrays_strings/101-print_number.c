81df9e4a9f003926cd127a427f9b6419566caf2c
zaynOm <ouaziz0626@gmail.com>
2023-03-30 17:43:47 +0000

    Task9: 101-print_number.c 💯

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -9,10 +9,16 @@ void print_number(int n)
 	if (n < 0)
 	{
 		_putchar('-');
+		if (n == -2147483648)
+		{
+			_putchar('2');
+			print_number(147483648);
+			return;
+		}
 		n *= -1;
 	}
 
-	if (n / 10 > 0)
+	if (n >= 10)
 		print_number(n / 10);
 
 	_putchar((n % 10) + 48);
