8e1fbb825fee9c50b5672f4b194416f04ea061b4
zaynOm <ouaziz0626@gmail.com>
2023-04-03 17:14:27 +0000

    Task3: 3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,30 @@
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: a string
+ * @accept: a string
+ *
+ * Return: the number of bytes in s which consist only of bytes from accept
+ *
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, sum = 0;
+
+	while (*accept)
+	{
+		i = 0;
+		while (s[i])
+		{
+			if (s[i] == *accept)
+			{
+				sum += 1;
+				break;
+			}
+			i++;
+		}
+		accept++;
+	}
+	return (sum + 1);
+
+}
