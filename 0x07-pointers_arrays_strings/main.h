403bbc16203080b8f3a82f641cb5b7ef24f1eb0a
zaynOm <ouaziz0626@gmail.com>
2023-04-04 00:44:54 +0000

    Task7: 7-print_chessboard.c :fire:

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H_
 #define MAIN_H_
 
-/*_putchar(char);*/
+int _putchar(char);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
