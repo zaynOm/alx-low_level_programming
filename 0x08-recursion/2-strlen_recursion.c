658072a079ac15601decf5ab73c6d3eea2e2419d
zaynOm <ouaziz0626@gmail.com>
2023-04-05 23:59:11 +0000

    Task2: 2-strlen_recursion.c :fire:

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,9 +1,28 @@
 /**
+ * length - finds the length of a string
+ * @s: a string
+ * @l: integer
  *
+ * Return: the length
+ */
+
+int length(char *s, int l)
+{
+	if (!*s)
+		return (l);
+
+	return (length(s + 1, l + 1));
+
+}
+
+/**
+ * _strlen_recursion - returns the length of s
+ * @s: string
  *
+ * Return: the lenght of s
  */
 int _strlen_recursion(char *s)
 {
-	
+	return (length(s, 0));
 
 }
