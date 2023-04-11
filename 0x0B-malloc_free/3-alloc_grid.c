90b0fd23b6bc5d4b959eb001b46f9a3ee40d3ab6
zaynOm <ouaziz0626@gmail.com>
2023-04-11 13:56:29 +0000

    Task3: :white_check_mark::fire:

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -21,7 +21,10 @@ int **alloc_grid(int width, int height)
 		grid[i] = malloc(sizeof(int) * width);
 
 	if (grid == NULL)
+	{
+		free(grid);
 		return (NULL);
+	}
 
 	for (i = 0; i < height; i++)
 	{
