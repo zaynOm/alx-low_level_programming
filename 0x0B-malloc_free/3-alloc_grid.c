f9ff44b28195fb25b72164fb64878f06cd463bdb
zaynOm <ouaziz0626@gmail.com>
2023-04-11 13:53:59 +0000

    Task3: :white_check_mark::fire:

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+/**
+ * alloc_grid - allocate a 2d array of integers
+ * @width: integer
+ * @height: integer
+ *
+ * Return: a pointer to the 2d array, NULL on failure or if
+ * one of the parameters is less then or equale to zero.
+ */
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+	int **grid;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	grid = malloc(sizeof(int *) * height);
+
+	for (i = 0; i < height; i++)
+		grid[i] = malloc(sizeof(int) * width);
+
+	if (grid == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+			grid[i][j] = 0;
+	}
+
+	return (grid);
+}
