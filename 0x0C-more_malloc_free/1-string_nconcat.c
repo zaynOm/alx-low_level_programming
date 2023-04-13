218640daaf67d9a50be8a900ce1bf1e911982c68
zaynOm <ouaziz0626@gmail.com>
2023-04-13 18:53:03 +0000

    Task1: :white_check_mark::fire:

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+/**
+ * _strlen - countes the length of a string
+ * @s: a string
+ *
+ * Return: the length
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+		i++;
+
+	return (i);
+}
+
+/**
+ * string_nconcat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ * @n: how much chars to take from s2
+ *
+ * Return: a pointer to the new string
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *str;
+	unsigned int i = 0, j = 0;
+
+	str = malloc(sizeof(*str) * (_strlen(s1) + n + 1));
+
+	while (s1[i])
+	{
+		str[i] = s1[i];
+		i++;
+	}
+
+	while (j < n)
+	{
+		str[i] = s2[j];
+		i++;
+		j++;
+	}
+	str[i] = '\0';
+
+	return (str);
+}
