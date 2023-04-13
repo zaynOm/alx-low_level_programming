11e7c67a874b3240e28d8a5bef19c1ac43cfff88
zaynOm <ouaziz0626@gmail.com>
2023-04-13 23:49:52 +0000

    Task2: white_check_mark::fire:

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -14,7 +14,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	ptr = malloc(nmemb * size);
+	ptr = malloc((nmemb * size) + 1);
 
 	if (!ptr)
 		return (NULL);
