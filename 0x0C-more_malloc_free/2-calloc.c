2d40d56f610f19e94f61d97b387051413f5a5843
zaynOm <ouaziz0626@gmail.com>
2023-04-13 23:43:59 +0000

    Task2: white_check_mark::fire:

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+/**
+ * _calloc - allocates memory for an array, using malloc
+ * @nmemb: number of elements
+ * @size: bytes needed for each element
+ *
+ * Return: a pointer to the allocated memory, NULL if nmemb or size is 0
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *ptr;
+	unsigned int i = 0;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	ptr = malloc(nmemb * size);
+
+	if (!ptr)
+		return (NULL);
+
+	while (ptr[i])
+	{
+		ptr[i] = 0;
+		i++;
+	}
+	return (ptr);
+}
