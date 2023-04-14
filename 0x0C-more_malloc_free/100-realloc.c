08d414a0fd6bab353521484339c917e9854f146a
zaynOm <ouaziz0626@gmail.com>
2023-04-14 01:30:23 +0000

    Task4 :100::rocket::fire:

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+/**
+ * _ptrncpy - copys the values from a pointer to another one
+ * @s: pointer to copy to
+ * @ptr: pointer to copy from
+ * @len: number of values to copy
+ */
+void _ptrncpy(char *s, char *ptr, int len)
+{
+	int i = 0;
+
+	while (i < len)
+	{
+		s[i] = ptr[i];
+		i++;
+	}
+}
+
+/**
+ * _realloc - reallocates a memory block using malloc and free
+ * @ptr: pointer to the memory previously allocated
+ * @old_size: the size of the allocated space for ptr
+ * @new_size: the new size of the new memory block
+ *
+ * Return: a pointer with the new size, NULL if new_size is 0 and ptr no null
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *s;
+	unsigned int len;
+
+	s = malloc(sizeof(char) * new_size + 1);
+
+	if (!s || new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	len = (old_size < new_size) ? old_size : new_size;
+	_ptrncpy(s, ptr, len);
+	free(ptr);
+	return (s);
+}
