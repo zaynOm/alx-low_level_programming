cd76a1a9e673f9496c77e80591c5395986151b2e
zaynOm <ouaziz0626@gmail.com>
2023-04-17 22:35:36 +0000

    Task0: :white_check_mark::fire:

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog with 3 members
+ * @name: dog name
+ * @age: dog age
+ * @owner: name of the dog owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
