b0332b4b6e83a830adae65fc91d91db919836cad
zaynOm <ouaziz0626@gmail.com>
2023-04-18 00:36:39 +0000

    Task4: first try 🥴

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+/**
+ * new_dog - creates a new dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ *
+ * Return: a pointer to a new dog
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t d;
+	
+	d = malloc(sizeof(dog_t));
+	d.name = name;
+	d.age = age;
+	d.owner = owner;
+
+	return (d);
+}
