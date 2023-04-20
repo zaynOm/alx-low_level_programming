77d352cbb23f7780e8d6ce26b866b1fd1b9a120c
zaynOm <ouaziz0626@gmail.com>
2023-04-20 17:24:36 +0000

    Task1: :white_check_mark::fire:

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,20 @@
+#include  "variadic_functions.h"
+/**
+ * print_numbers - prints numbers, followed by a new line
+ * @separator: the string to be printed between numbers
+ * @n: number of the  passed integers
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+
+	va_start(ap, n);
+
+	for (i = 0; i < n - 1; i++)
+		printf("%d%s", va_arg(ap, int), separator);
+
+	printf("%d\n", va_arg(ap, int));
+
+	va_end(ap);
+}
