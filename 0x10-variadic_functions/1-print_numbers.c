95209eb55558a76532b59cafce891118fe71c97b
zaynOm <ouaziz0626@gmail.com>
2023-04-20 17:34:43 +0000

    Task1: :white_check_mark::fire:

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -12,9 +12,13 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_start(ap, n);
 
 	for (i = 0; i < n - 1; i++)
-		printf("%d%s", va_arg(ap, int), separator);
+	{
+		if (separator)
+			printf("%d%s", va_arg(ap, int), separator);
+		else
+			printf("%d", va_arg(ap, int));
+	}
 
 	printf("%d\n", va_arg(ap, int));
-
 	va_end(ap);
 }
