04fa8db054221d1e7a8605c08ebc500121648753
zaynOm <ouaziz0626@gmail.com>
2023-04-28 00:43:41 +0100

    task5: :100::fire:

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * first - a function that will automatically run befor main
+ */
+void  __attribute__((constructor)) first()
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+
+}
