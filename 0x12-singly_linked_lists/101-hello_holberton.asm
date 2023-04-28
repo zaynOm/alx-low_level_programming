9ee306fda3c7a157d3fcc91b62b7a92418dec159
zaynom <ouaziz0626@gmail.com>
2023-04-27 19:50:53 -0700

    task6: :100::rocket::fire::exploding_head:

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,17 +1,13 @@
-section .data
-	msg: db "Hello, Holberton", 10
-
+	msg db "Hello, Holberton", 10, 0
 section .text
 	global main
-
 	extern printf
-	
-	main:
-		push msg
-		call printf
-		add esp, 4
+main:
+	push rbp
+	mov rdi,msg
+	mov rax,0
+	call printf
 
-		mov eax, 1
-		xor ebx, ebx
-		int 0x80
-	
+	pop rbp
+	mov rax,0
+	ret
