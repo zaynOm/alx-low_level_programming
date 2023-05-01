fa6a5a856cb789c22712de4cdda451a3a1dfda63
zaynOm <ouaziz0626@gmail.com>
2023-05-01 15:53:44 +0100

    task4: ✅🚀

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,13 +1,13 @@
 #include "lists.h"
 
 /**
+ * free_listint - frees a listint_t list.
+ * @head:  pointer to the first node (head)
  */
 void free_listint(listint_t *head)
 {
-	if (!head->next)
+	if (!head)
 		return;
-
 	free_listint(head->next);
-
-	free(head->next);
+	free(head);
 }
