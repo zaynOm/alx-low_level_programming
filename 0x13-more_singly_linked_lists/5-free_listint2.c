abc4eb5a7e8e3b188605fc1fc74d6a46461441d9
zaynOm <ouaziz0626@gmail.com>
2023-05-01 16:40:50 +0100

    task5: ✅🚀

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a listint_t list & sets the head to NULL
+ * @head: pointer to the first node (head)
+ */
+void free_listint2(listint_t **head)
+{
+	if (!*head)
+		return;
+
+	free_listint2(&(*head)->next);
+	free(*head);
+	*head = NULL;
+}
