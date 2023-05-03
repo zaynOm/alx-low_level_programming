7962d7a691feaaf15939a994b6f2b5e9cebdfc72
zaynOm <ouaziz0626@gmail.com>
2023-05-03 17:15:38 +0100

    task12: :100:✅🚀

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,6 +1,36 @@
 #include "lists.h"
 
 /**
+ * find_listint_loop - Finds the loop in a linked list.
+ * @head: pointer to the first node (head)
+ *
+ * Return: The address of the node where the loop starts,
+ * or NULL if there is no loop
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *slow = head, *fast = head;
+
+	while (slow && fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+
+		if (slow == fast)
+		{
+			slow = head;
+			while (slow != fast)
+			{
+				slow = slow->next;
+				fast = fast->next;
+			}
+			return (slow);
+		}
+	}
+
+	return (NULL);
+}
+/**
  * print_listint_safe - Prints a listint_t linked list, even if it has a loop
  * @head: Pointer to the head of the list
  *
@@ -8,25 +38,23 @@
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *prev, *node;
+	listint_t *loop;
 	size_t count = 0;
+	int isloop = 1;
 
 	if (!head)
 		exit(98);
-	node = head;
-	while (node)
+
+	loop = find_listint_loop((listint_t *)head);
+	for (isloop = 1; (head != loop || isloop) && head;)
 	{
-		printf("[%p] %d\n", (void *)node, node->n);
+		printf("[%p] %d\n", (void *)head, head->n);
 		count++;
-		prev = node;
-		node = node->next;
-
-		if (node > prev)
-		{
-			printf("-> [%p] %d\n", (void *)node, node->n);
-			break;
-		}
-
+		if (head == loop)
+			isloop = 0;
+		head = head->next;
 	}
+	if (loop)
+		printf("-> [%p] %d\n", (void *)head, head->n);
 	return (count);
 }
