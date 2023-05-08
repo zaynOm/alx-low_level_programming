fbee3cb6f40c8cadccebfdeeb5ede41cabf5512e
zaynOm <ouaziz0626@gmail.com>
2023-05-09 00:16:35 +0100

    task0: ✅🚀

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: pointer to a string containing the file name
+ * @letters: the number of letters it should read and print
+ *
+ * Return: the number of letters it could read and print, 0 if an error occurs
+ */
+size_t read_textfile(const char *filename, size_t letters)
+{
+	char *buff;
+	int fop, fre, fwr;
+
+	buff = malloc(sizeof(char) * letters);
+	if (!filename || !buff)
+		return (0);
+
+	fop = open(filename, O_RDONLY);
+	fre = read(fop, buff, letters);
+	fwr = write(1, buff, fre);
+
+	if (fop * fre * fwr < 0 || fre != fwr)
+	{
+		free(buff);
+		return (0);
+	}
+	free(buff);
+	close(fop);
+	return (fwr);
+}
