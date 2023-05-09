6569da555a647765696d662d429dc4c362664ff6
zaynOm <ouaziz0626@gmail.com>
2023-05-09 02:28:52 +0100

    task2: ✅🚀

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -15,8 +15,8 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (text_content)
 	{
-		while (text_content[len++])
-			;
+		while (text_content[len])
+			len++;
 	}
 	fd = open(filename, O_WRONLY | O_APPEND);
 	bytes = write(fd, text_content, len);
