9d41878d03a4bc5cfda021adb62ac60336d1ea6f
zaynOm <ouaziz0626@gmail.com>
2023-05-09 02:11:15 +0100

    task1: ✅🚀

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,9 +13,11 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-
-	while (text_content[len])
+	if (text_content)
+	{
+		while (text_content[len])
 		len++;
+	}
 
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	bytes = write(fd, text_content, len);
