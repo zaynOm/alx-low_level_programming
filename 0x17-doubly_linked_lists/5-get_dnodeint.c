b99a3bf7c22b4fe61bd93b436635f141383d0f7c
zaynOm <ouaziz0626@gmail.com>
2023-06-15 21:39:09 +0100

    task 5: :fire:

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - searches for a node at the list
+ * @head: pointer to the head of dlistint_t
+ * @index: the index of the node, starting from 0
+ *
+ * Return: node at @index, or NULL if it does not exists
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	int idx = index;
+
+	while (head && idx--)
+		head = head->next;
+
+	if (!head && idx >= 0)
+		return (NULL);
+
+	return (head);
+}
