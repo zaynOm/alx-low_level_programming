6d96b054af396bca497951ecb0a33d5dd5200dc3
zaynOm <ouaziz0626@gmail.com>
2023-06-20 02:25:39 +0100

    💉💉 dynamic lib containing a custom rand function

---
+++ 0x18-dynamic_libraries/myrand.c
@@ -0,0 +1,13 @@
+/**
+ * rand - custom rand to win the Giga Millions
+ *
+ * Return: a number from the list each time it's called
+ */
+int rand()
+{
+	static int i;
+	int nums[8] = {8, 8, 7, 9, 23, 74};
+
+	return (unsigned int)nums[i++];
+}
+
