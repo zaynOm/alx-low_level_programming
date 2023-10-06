3c04ed584328cbcd88a1f3264beed6c13525fa35
zaynOm <ouaziz0626@gmail.com>
2023-10-06 16:51:31 +0100

    task 0: >>> ht = {}

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,24 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a hash table.
+ * @size: the size of the array.
+ *
+ * Return: the new created table.
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	int i;
+	hash_table_t *table = malloc(sizeof(hash_table_t));
+
+	if (!table)
+		return (NULL);
+
+	table->size = size;
+	table->array = (hash_node_t **)calloc(size, sizeof(hash_node_t *));
+
+	for (i = 0; i < size; i++)
+		table->array[i] = NULL;
+
+	return (table);
+}
