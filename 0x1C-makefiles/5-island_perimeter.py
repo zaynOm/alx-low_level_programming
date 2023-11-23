40fef5ab74ffb603a2e7d1e63be7249860c5c2e7
zaynom <ouaziz0626@gmail.com>
2023-11-22 18:12:09 -0800

    task 5: Island Perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -13,17 +13,21 @@ def island_perimeter(grid):
     """
 
     prmtr = 0
-
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
+    lenght = len(grid)
+    bottom = lenght - 1
+    for i in range(lenght):
+        right = len(grid[i]) - 1
+        for j in range(right + 1):
             if grid[i][j] == 1:
                 if i > 0 and grid[i - 1][j] == 0:
                     prmtr += 1
                 if j > 0 and grid[i][j - 1] == 0:
                     prmtr += 1
-                if i < len(grid) - 1 and grid[i + 1][j] == 0:
+                if i < bottom and grid[i + 1][j] == 0:
                     prmtr += 1
-                if j < len(grid[i]) - 1 and grid[i][j + 1] == 0:
+                if j < right and grid[i][j + 1] == 0:
+                    prmtr += 1
+                if i == 0 or j == 0 or i == bottom or j == right:
                     prmtr += 1
 
     return prmtr
